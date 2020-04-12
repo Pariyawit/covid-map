581ab05f611def0af4a15712b6d4fb678918bf6f
Pariyawit Jatukannyaprateep <pariyawit.jat@gmail.com>
2020-04-12 14:44:45 +1000

    add info animation

--- src/components/Info.js
+++ src/components/Info.js
@@ -3,22 +3,29 @@ import { CaseContext } from './../context/CaseContext';
 
 export default function Info() {
   const { country, showInfo } = useContext(CaseContext);
-  return country && showInfo ? (
-    <div className='info card' style={{ zIndex: 1000 }}>
-      <h3 className='info__title'>
-        {country.province && `${country.province}, `}
-        {country.country}
-      </h3>
-      <p className='info__description'>
-        Confirmed: {country.latest.confirmed.toLocaleString()}
-        <br />
-        Deaths: {country.latest.deaths.toLocaleString()}
-      </p>
-      <p className='info__lastUpdate'>
-        Last Updated: {country.last_updated.slice(0, 19).replace('T', ' ')} UTC
-      </p>
+
+  const infoClass = showInfo ? 'info card' : 'info card hide';
+  return (
+    <div className={infoClass} style={{ zIndex: 1000 }}>
+      {country && showInfo ? (
+        <>
+          <h3 className='info__title'>
+            {country.province && `${country.province}, `}
+            {country.country}
+          </h3>
+          <p className='info__description'>
+            Confirmed: {country.latest.confirmed.toLocaleString()}
+            <br />
+            Deaths: {country.latest.deaths.toLocaleString()}
+          </p>
+          <p className='info__lastUpdate'>
+            Last Updated: {country.last_updated.slice(0, 19).replace('T', ' ')}{' '}
+            UTC
+          </p>
+        </>
+      ) : (
+        ''
+      )}
     </div>
-  ) : (
-    ''
   );
 }
